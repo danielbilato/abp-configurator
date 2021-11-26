2f7fd91364d22ffaeb8fba6f36b134e908ac4057
Daniel <daniel.bilato@gmail.com>
2021-11-26 08:21:26 +0100

    Primo commit

---
+++ angular/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+﻿import { FrameworkPage } from './app.po';
+
+describe('abp-zero-template App', function () {
+    let page: FrameworkPage;
+
+    beforeEach(() => {
+        page = new FrameworkPage();
+    });
+
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        page.getCopyright().then(value => {
+            expect(value).toEqual(new Date().getFullYear() + ' © Framework.');
+        });
+    });
+});
